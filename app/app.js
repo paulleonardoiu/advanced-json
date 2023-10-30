5588c47513a1c4da3e179273a11340dd2dd80c4c
paulleonardoiu <pauleon@iu.edu>
2023-10-29 21:07:37 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,520 @@
+var LISTS = [
+    {
+       "name":"Groceries",
+       "listItems":[
+          {
+             "name":"milk",
+             "checked":false
+          },
+          {
+             "name":"cheese",
+             "checked":false
+          },
+          {
+             "name":"oranges",
+             "checked":false
+          },
+          {
+            "name":"lettuce",
+            "checked":false
+         },
+         {
+            "name":"tomatoes",
+            "checked":false
+         },
+         {
+            "name":"bread",
+            "checked":false
+         },
+         {
+            "name":"spoons",
+            "checked":false
+         },
+         {
+            "name":"forks",
+            "checked":false
+         },
+         {
+            "name":"napkins",
+            "checked":false
+         },
+         {
+            "name":"ham",
+            "checked":false
+         }
+       ]
+    },
+    {
+        "name":"Camping",
+        "listItems":[
+           {
+              "name":"tent",
+              "checked":false
+           },
+           {
+              "name":"pillow",
+              "checked":false
+           },
+           {
+              "name":"sleeping bag",
+              "checked":false
+           },
+           {
+             "name":"bug spray",
+             "checked":false
+          },
+          {
+             "name":"water bottle",
+             "checked":false
+          },
+          {
+             "name":"hatchet",
+             "checked":false
+          },
+          {
+             "name":"lighter",
+             "checked":false
+          },
+          {
+             "name":"fishing pole",
+             "checked":false
+          },
+          {
+             "name":"lantern",
+             "checked":false
+          },
+          {
+             "name":"head lamp",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Homework",
+        "listItems":[
+           {
+              "name":"chemistry hw #1",
+              "checked":false
+           },
+           {
+              "name":"chemistry exam",
+              "checked":false
+           },
+           {
+              "name":"reading assignment pg 412-450",
+              "checked":false
+           },
+           {
+             "name":"math homework",
+             "checked":false
+          },
+          {
+             "name":"english paper",
+             "checked":false
+          },
+          {
+             "name":"spanish homework",
+             "checked":false
+          },
+          {
+             "name":"economics presentation",
+             "checked":false
+          },
+          {
+             "name":"coding homework 7",
+             "checked":false
+          },
+          {
+             "name":"coding week 2 assessment",
+             "checked":false
+          },
+          {
+             "name":"debate class after school",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Strawberry Cake Recipe",
+        "listItems":[
+           {
+              "name":"1 lb of fresh strawberries",
+              "checked":false
+           },
+           {
+              "name":"2 1/2 cups of cake flour",
+              "checked":false
+           },
+           {
+              "name":"2 teaspoons of baking powder",
+              "checked":false
+           },
+           {
+             "name":"1/2 teaspoon baking soda",
+             "checked":false
+          },
+          {
+             "name":"1 teaspoon salt",
+             "checked":false
+          },
+          {
+             "name":"3/4 cup unsalted butter",
+             "checked":false
+          },
+          {
+             "name":"1 3/4 cups granulated sugar",
+             "checked":false
+          },
+          {
+             "name":"5 large egg whites",
+             "checked":false
+          },
+          {
+             "name":"1/3 cup sour cream",
+             "checked":false
+          },
+          {
+             "name":"2 teaspoons vanilla extract",
+             "checked":false
+          },
+          {
+            "name":"1/2 cup whole milk",
+            "checked":false
+         },
+         {
+            "name":"1/2 cup reduced strawberry puree",
+            "checked":false
+         }
+        ]
+     },
+     {
+        "name":"Video Games",
+        "listItems":[
+           {
+              "name":"Halo",
+              "checked":false
+           },
+           {
+              "name":"Apex Legends",
+              "checked":false
+           },
+           {
+              "name":"Call of Duty",
+              "checked":false
+           },
+           {
+             "name":"Valorant",
+             "checked":false
+          },
+          {
+             "name":"Overwatch",
+             "checked":false
+          },
+          {
+             "name":"Aimlabs",
+             "checked":false
+          },
+          {
+             "name":"Super Monkey Ball",
+             "checked":false
+          },
+          {
+             "name":"Minecraft",
+             "checked":false
+          },
+          {
+             "name":"Roblox",
+             "checked":false
+          },
+          {
+             "name":"Animal Crossing",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Books",
+        "listItems":[
+           {
+              "name":"7 Habits of Highly Effective People",
+              "checked":false
+           },
+           {
+              "name":"The Millionaire Mind",
+              "checked":false
+           },
+           {
+              "name":"The Millionaire Next Door",
+              "checked":false
+           },
+           {
+             "name":"The Richest Man in Babylon",
+             "checked":false
+          },
+          {
+             "name":"Discipline is Destiny",
+             "checked":false
+          },
+          {
+             "name":"How to Win Friends and Influence People",
+             "checked":false
+          },
+          {
+             "name":"Atomic Habits",
+             "checked":false
+          },
+          {
+             "name":"Rich Dad Poor Dad",
+             "checked":false
+          },
+          {
+             "name":"Never Split the Difference",
+             "checked":false
+          },
+          {
+             "name":"Emotional Intelligence",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Budget",
+        "listItems":[
+           {
+              "name":"$300 Car Payment",
+              "checked":false
+           },
+           {
+              "name":"$800 Rent",
+              "checked":false
+           },
+           {
+              "name":"$200 Groceries",
+              "checked":false
+           },
+           {
+             "name":"$120 Phone Bill",
+             "checked":false
+          },
+          {
+             "name":"$35 Gym Membership",
+             "checked":false
+          },
+          {
+             "name":"$60 Subscriptions",
+             "checked":false
+          },
+          {
+             "name":"$40 Electricity Bill",
+             "checked":false
+          },
+          {
+             "name":"$40 Water Bill",
+             "checked":false
+          },
+          {
+             "name":"$100 Gas",
+             "checked":false
+          },
+          {
+             "name":"$200 Entertainment",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Halloween Decorations",
+        "listItems":[
+           {
+              "name":"Spider Webs",
+              "checked":false
+           },
+           {
+              "name":"Candy Bown",
+              "checked":false
+           },
+           {
+              "name":"Bat Stickers",
+              "checked":false
+           },
+           {
+             "name":"Smoke Machine",
+             "checked":false
+          },
+          {
+             "name":"Scary Music",
+             "checked":false
+          },
+          {
+             "name":"RGB Lights",
+             "checked":false
+          },
+          {
+             "name":"Pumpkins",
+             "checked":false
+          },
+          {
+             "name":"Candy",
+             "checked":false
+          },
+          {
+             "name":"Take 1 Sign",
+             "checked":false
+          },
+          {
+             "name":"Skeleton",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Christmas Gifts",
+        "listItems":[
+           {
+              "name":"Mom",
+              "checked":false
+           },
+           {
+              "name":"Dad",
+              "checked":false
+           },
+           {
+              "name":"Sister",
+              "checked":false
+           },
+           {
+             "name":"Brother",
+             "checked":false
+          },
+          {
+             "name":"Girlfriend",
+             "checked":false
+          },
+          {
+             "name":"Aunt",
+             "checked":false
+          },
+          {
+             "name":"Uncle",
+             "checked":false
+          },
+          {
+             "name":"Girlfriend's Mom",
+             "checked":false
+          },
+          {
+             "name":"Girlfriend's Dad",
+             "checked":false
+          },
+          {
+             "name":"GIrlfriend's Brother",
+             "checked":false
+          }
+        ]
+     },
+     {
+        "name":"Chores",
+        "listItems":[
+           {
+              "name":"Laundry",
+              "checked":false
+           },
+           {
+              "name":"Dishes",
+              "checked":false
+           },
+           {
+              "name":"Clean Room",
+              "checked":false
+           },
+           {
+             "name":"Clean Kitchen",
+             "checked":false
+          },
+          {
+             "name":"Clean Living Room",
+             "checked":false
+          },
+          {
+             "name":"Clean Family Room",
+             "checked":false
+          },
+          {
+             "name":"Sweep Floor",
+             "checked":false
+          },
+          {
+             "name":"Mop Floor",
+             "checked":false
+          },
+          {
+             "name":"Light Candles",
+             "checked":false
+          },
+          {
+             "name":"Feed Dogs",
+             "checked":false
+          }
+        ]
+     }
+ ];
+
+function addItem(listIndex){
+    let newItemName = $("#addItem").val();
+    let newItemObj = {
+        name: newItemName,
+        checked: false
+    }
+    LISTS[listIndex].listItems.push(newItemObj);
+    loadListItems(listIndex);
+    // console.log(LISTS);
+}
+
+function itemChecked(element, listIndex, itemIndex){
+    $(element).parent().toggleClass("strike");
+    let checkedValue = !LISTS[listIndex].listItems[itemIndex].checked;
+    LISTS[listIndex].listItems[itemIndex].checked = checkedValue;
+    console.log("checked: " + checkedValue);
+}
+
+function deleteItem(listIndex, idx){
+    LISTS[listIndex].listItems.splice(idx, 1);
+    loadListItems(listIndex);
+}
+
+function loadListItems(listIndex){
+    $('#title').html(LISTS[listIndex].name + " List Details");
+    let listString = `<button class="btn" onclick="loadLists()">Back</button><ul>`;
+    $.each(LISTS[listIndex].listItems, function(idx, listItem){
+        listString += `<li id="${idx}" class="${listItem.checked ? "strike" : ""}">
+        <input ${listItem.checked ? (checked = "checked"): ""} type="checkbox" id="${idx}" name="${listItem.name}" onclick="itemChecked(this, ${listIndex}, ${idx})">
+        <span>${listItem.name}</span><span class="delete" onclick="deleteItem(${listIndex}, ${idx})">Delete</span>
+        </li>`;
+    });
+    listString += `</ul>
+    <div class="addItemInput">
+        <input type="text" id="addItem">
+        <button class="btn" onclick="addItem(${listIndex})">Add Item</button>
+    </div>`;
+    $("#app").html(listString);
+}
+
+function loadLists(){
+    $('#title').html("Check Lists");
+    let listString = "<ul>";
+    $.each(LISTS, function(idx, list){
+        listString += `<li id="${idx}" onclick="loadListItems(${idx})">${list.name}<span class="right">Items: ${list.listItems.length}</span></li>`;
+    });
+    listString += "</ul>";
+    $("#app").html(listString);
+}
+
+function initListeners(){
+    $("#loadList").on("click",function(){
+        loadLists();
+    });
+}
+
+$(document).ready(function(){
+    initListeners();
+})
